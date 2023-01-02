a07b208b153b365fb6e7a1f438920198ca47c94d
Paul <paulglujan@gmail.com>
2023-01-02 11:58:22 -0800

    Finished button styling

--- custom-navigation/src/App.jsx
+++ custom-navigation/src/App.jsx
@@ -1,22 +1,88 @@
 import Button from "./Button";
+import { GoBell, GoSmiley, GoListUnordered } from "react-icons/go";
 
 function App() {
   return (
-    <div>
+    <div className="columns-3">
       <div>
-        <Button primary>Click Me!</Button>
+        <div>
+          <Button primary>
+            <GoBell />
+            Click Me!
+          </Button>
+        </div>
+        <div>
+          <Button secondary>
+            <GoSmiley />
+            Buy Now!
+          </Button>
+        </div>
+        <div>
+          <Button success>
+            <GoListUnordered />
+            See Deals!
+          </Button>
+        </div>
+        <div>
+          <Button warning>Hide Ads!</Button>
+        </div>
+        <div>
+          <Button danger>Something!</Button>
+        </div>
       </div>
       <div>
-        <Button>Buy Now!</Button>
+        <div>
+          <Button primary rounded>
+            Click Me!
+          </Button>
+        </div>
+        <div>
+          <Button secondary rounded>
+            Buy Now!
+          </Button>
+        </div>
+        <div>
+          <Button success rounded>
+            See Deals!
+          </Button>
+        </div>
+        <div>
+          <Button warning rounded>
+            Hide Ads!
+          </Button>
+        </div>
+        <div>
+          <Button danger rounded>
+            Something!
+          </Button>
+        </div>
       </div>
       <div>
-        <Button>See Deals!</Button>
-      </div>
-      <div>
-        <Button>Hide Ads!</Button>
-      </div>
-      <div>
-        <Button>Something!</Button>
+        <div>
+          <Button primary outline>
+            Click Me!
+          </Button>
+        </div>
+        <div>
+          <Button secondary outline>
+            Buy Now!
+          </Button>
+        </div>
+        <div>
+          <Button success outline>
+            See Deals!
+          </Button>
+        </div>
+        <div>
+          <Button warning outline>
+            Hide Ads!
+          </Button>
+        </div>
+        <div>
+          <Button danger outline>
+            Something!
+          </Button>
+        </div>
       </div>
     </div>
   );
--- custom-navigation/src/Button.jsx
+++ custom-navigation/src/Button.jsx
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+import classNames from "classnames";
 
 const Button = ({
   children,
@@ -10,7 +10,22 @@ const Button = ({
   outline,
   rounded,
 }) => {
-  return <button>{children}</button>;
+  const classes = classNames("flex items-center gap-x-1 border px-3 py-1.5", {
+    "border-blue-600 bg-blue-500 text-white": primary,
+    "border-gray-600 bg-gray-500 text-white": secondary,
+    "border-green-600 bg-green-500 text-white": success,
+    "border-yellow-600 bg-yellow-500 text-white": warning,
+    "border-red-600 bg-red-500 text-white": danger,
+    "rounded-full": rounded,
+    "bg-white": outline,
+    "text-blue-500": outline && primary,
+    "text-gray-500": outline && secondary,
+    "text-green-500": outline && success,
+    "text-yellow-500": outline && warning,
+    "text-red-500": outline && danger,
+  });
+
+  return <button className={classes}>{children}</button>;
 };
 
 Button.propTypes = {
