25f158e3992e4bd273c6e29097697b234203baf8
Paul <paulglujan@gmail.com>
2023-01-05 14:54:22 -0800

    Done with dropdown for real this time

--- custom-navigation/src/components/Dropdown.jsx
+++ custom-navigation/src/components/Dropdown.jsx
@@ -1,9 +1,30 @@
-import { useState } from "react";
-import { GoChevronDown, GoTag } from "react-icons/go";
+import { useEffect, useRef, useState } from "react";
+import { GoChevronDown } from "react-icons/go";
 import Panel from "./Panel";
 
 const Dropdown = ({ options, value, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const divEl = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (!divEl) {
+        return;
+      }
+
+      if (!divEl.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handler, true);
+
+    const cleanUp = () => {
+      document.removeEventListener("click", handler);
+    };
+
+    return cleanUp;
+  }, []);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
@@ -29,7 +50,7 @@ const Dropdown = ({ options, value, onChange }) => {
   };
 
   return (
-    <div className="relative w-48">
+    <div className="relative w-48" ref={divEl}>
       <Panel
         className="flex cursor-pointer items-center justify-between"
         onClick={handleClick}
